d6180a70eddee2dd2f48f37d7bfb81272a5c0b73
Hiradjoh <hiradjo021@gmail.com>
2025-10-07 11:11:37 +0330

    Web API Versioning

---
+++ My-books/Controllers/V1/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_books.Controllers.V1
+{
+  /*  [ApiVersion ("1.0")]*/ // query versioning 
+    [Route("api/[controller]")]
+
+    //[Route("api/v{version:apiVersion}/[controller]")] // url versioning 
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("get-test-data")]
+        public IActionResult Get()
+        {
+            return Ok("this is test controller V1 ");
+        }
+         
+
+    }
+}
